b16c7b4097dcbaca92f9f31dccbce90577def227
SanzGantengz <81704601+SanzGantengz@users.noreply.github.com>
2021-12-02 17:26:19 +0700

    Update instagram.js

--- instagram.js
+++ instagram.js
@@ -10,13 +10,17 @@ const selector = {
     verify: {
       code: "//button[text() = 'Send Security Code']",
       submit: "//button[text() = 'Submit']",
-      type: "input[name=security_code]",
+      type: "input[name=security._code]",
       timeout: 2000
     }
   },
   follow: {
     selector: "//button[text() =  'Follow']",
     timeout: 1000
+  },
+  message: {
+    TYPE_AND_SEND: "svg[aria-label=Messenger]",
+    send: "\u000d"
   }
 }
 function Client(options)
@@ -30,7 +34,7 @@ function Client(options)
 
 Client.prototype.login = function login(username, password, code)
 {
-  new Primise(async(resolve) => {
+  new Promise(async(resolve) => {
     try {
     var browser = await puppeteer.launch(this.OPTIONS.PUPPETEER)
     this.browser = browser;
@@ -39,7 +43,7 @@ Client.prototype.login = function login(username, password, code)
     if (fs.existsSync(this.PATH_SESSION))
     {
       await this.page.goto(
-      "https://www.instagram.com/"
+      "https://www.instagram.com/accounts/login/"
       )
       await page.waitForSelector(selector.login.username)
       await this.page.type(selector.login.username, username)
@@ -108,9 +112,13 @@ Client.prototype.follow = function follow(username)
             if (follow)
             {
               await follow.click()
+              var pp_url = await this.page.evaluate(() => {
+                return document.querySelector("img").src
+              })
               resolve({
                 status: 200,
-                title: this.page.title() 
+                title: this.page.title(),
+                pp_url
               })
             }
             else {
@@ -136,14 +144,77 @@ Client.prototype.follow = function follow(username)
   })
 }
 Client.prototype.actifity = function actifity()
-
+{
+  new Promise(async(resolve, reject) => {
+    if (fs.existsSync(this.PATH_SESSION))
+    {
+      try {
+        await this.page.goto(
+          "https://www.instagram.com/accounts/activity/"
+          )
+          resolve({
+            status: true,
+            message: {
+              message: "soon",
+              image: {
+                path: "actifity.png",
+                buffer: this.page.screenshot({ path: "actifity.png" })
+              }
+            }
+          })
+      } 
+      catch (e) {
+        reject(e)
+      }
+    } 
+    else {
+      reject({
+        status: false,
+        message: "login first"
+      })
+    }
+  })
+}
+Client.prototype.dm = function dm(USERNAME_OR_ID, message, type)
+{
+  new Promise(async(resolve, reject) => {
+    if (fs.existsSync(this.PATH_SESSION))
+    {
+      try {
+        if (USERNAME_OR_ID) reject(new Error("invalid username or id"))
+        let cookie = fs.readFileSync(this.PATH_SESSION)
+        for (let cookies of cookie)
+        {
+          await this.page.setCookie(cookies)
+        }
+        await page.goto(
+          "https://www.instagram.com/" + USERNAME_OR_ID
+          )
+          if (type === "text")
+          {
+            await page.type(selector.message.TYPE_AND_SEND, message)
+           await page.type(selector.message.TYPE_AND_SEND, selector.message.send)
+          } else if (type === "media")
+          {
+            reject("blom jadi")
+          }
+      } catch (e) {
+        reject(e)
+      }
+    } else {
+      reject({
+        status: false,
+        message: "login first"
+      })
+    }
+  })
+}
 
 exports.Client = function client(options){
   try {
-    let c = new Client(options)
-    return c
+    return new Client(options)
   } catch (e) {
     console.log(e)
-    return e.message
+    return e
   }
 }
